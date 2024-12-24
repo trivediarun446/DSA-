f4822645d70a2698fd9bbbca398ea4522375e0d6
Arun <trivediarun446@gmail.com>
2024-12-24 23:13:57 +0530

    Some files are renamed

---
+++ Queue/Queue.cpp
@@ -0,0 +1,18 @@
+// Write programe to implement queue using cpp 
+
+#include<iostream>
+using namespace std ; 
+
+class Queue {
+    public:
+        int size ; 
+        int front ; 
+        int rear ; 
+        
+
+};
+
+int main ()
+{
+
+}
